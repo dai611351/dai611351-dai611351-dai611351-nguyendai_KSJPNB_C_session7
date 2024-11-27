d4a915c49ba22c5f951417ee80c08401973dd389
dai611351 <dai611351@gmail.com>
2024-11-28 00:30:58 +0700

    bai5

---
+++ Session07-5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int array[5] = {9, 13, 17, 24, 36};
+	int max = array[0];
+    int min = array[0];
+	for (int i = 1; i < 5; i++) {
+	        if (array[i] > max) {
+	            max = array[i]; 
+	        }
+	        if (array[i] < min) {
+	            min = array[i]; 
+	        }
+	    }
+	    printf("Phan tu lon nhat trong mang: %d\n", max);
+	    printf("Phan tu nho nhat trong mang: %d\n", min);
+	return 0;
+}
